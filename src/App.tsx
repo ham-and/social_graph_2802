e4ba168ddef8f42a086b7cf922c42b3e808e0eda
Polina Kutilina <polina.kutilina@soundcloud.com>
2025-02-28 16:08:52 +0100

    hardcode redirect uri again

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Search, Users, UserPlus, UsersRound, Loader2, AlertCircle, ChevronDown,
 // Constants for OAuth
 const CLIENT_ID = 'PKSAtFElFd989tO0uR1La2nk2es8Jupo';
 // const REDIRECT_URI = 'https://lively-mandazi-675d66.netlify.app/.netlify/functions/auth';
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+// const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+const REDIRECT_URI = 'https://polina-lively-mandazi-675d66.netlify.app/.netlify/functions/auth';
 
 declare type Dict<T> = { [key: string]: T | undefined };
 
